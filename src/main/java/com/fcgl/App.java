acfe4a86902023ff9ca8c4ba5472fbcaa8384719
Jean Paul Torre <jean.paul.ruben@gmail.com>
2018-08-19 20:40:01 -0400

    Deleted unused imports from App

--- src/main/java/com/fcgl/App.java
+++ src/main/java/com/fcgl/App.java
@@ -3,29 +3,8 @@
  */
 package com.fcgl;
 
-import com.sun.prism.impl.Disposer;
-import org.jooq.Record;
-import org.jooq.Result;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
-
-//import org.jooq.generated.tables.ItemConditionRecord;
-//import static test.generated.Tables.*;
-
-//import static test.generated.Tables.*;
-import org.jooq.DSLContext;
-import org.jooq.impl.DSL;
-import static org.jooq.generated.Tables.*;
-import org.jooq.SQLDialect;
-
-
-import java.sql.*;
-
-
-//import java.sql.Connection;
-//import java.sql.DriverManager;
 
 
 @SpringBootApplication
