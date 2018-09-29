56fd139a3a3fe9f7368603652045edb073d0485c
Thien Nguyen <thienn503@gmail.com>
2018-09-29 19:38:59 -0400

    SB-1 Added the methods to increase and decrease invntory

--- src/main/java/com/fcgl/Listing/Vendors/model/Inventory.java
+++ src/main/java/com/fcgl/Listing/Vendors/model/Inventory.java
@@ -17,4 +17,43 @@ public class Inventory {
     return this.quantity;
   }
 
+  private void setQuantity(int quantity) {
+    this.quantity = quantity;
+  }
+
+  /**
+   *
+   * The new total inventory quantity.
+   * @param increaseBy the number of quantity to be added
+   * @return
+   */
+  public int increaseQuantity(int increaseBy) {
+    if (increaseBy < 0) {
+      final String errorMsgTemplate = "Quantity to increase is negative : %d";
+      throw new IllegalArgumentException(String.format(errorMsgTemplate,increaseBy));
+    }
+    this.setQuantity(this.getQuantity() + increaseBy);
+    return this.getQuantity();
+  }
+
+  /**
+   * The new total inventory quantity.
+   * @param decreaseBy the number of quantity to be taking away
+   * @return the
+   */
+  public int decreaseQuantity(int decreaseBy) {
+    if (decreaseBy < 0) {
+      final String errorMsgTemplate = "Quantity to decreaseBy is negative: %d";
+      throw new IllegalArgumentException(String.format(errorMsgTemplate,decreaseBy));
+    }
+
+    int temp = this.getQuantity() - decreaseBy;
+    if (temp < 0) {
+      final String errorMsgTemplate = "Not enough quantity, Current %d; Quantity to be decrease %d";
+      throw new IllegalArgumentException(String.format(errorMsgTemplate, this.getQuantity(), decreaseBy));
+    }
+    this.setQuantity(this.getQuantity() - decreaseBy);
+    return this.getQuantity();
+  }
+
 }
