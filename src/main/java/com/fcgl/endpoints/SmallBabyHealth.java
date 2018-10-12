a0bc5ac2e8b1e09d49ccca1375b339abba91ec91
Jean Paul Torre <jean.paul.ruben@gmail.com>
2018-10-11 22:13:01 -0400

    SB-Bug gradle wasn't building because of logger. fixed it and added a log to the small baby health endpoint

--- src/main/java/com/fcgl/endpoints/SmallBabyHealth.java
+++ src/main/java/com/fcgl/endpoints/SmallBabyHealth.java
@@ -5,9 +5,8 @@ package com.fcgl.endpoints;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-//@RequestMaping maps / to the health() method
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Produces a simple endpoint to check that the server is alive
@@ -17,6 +16,8 @@ public class SmallBabyHealth {
 
   @RequestMapping("/googoo")
   public ResponseEntity health() {
+    Logger logger = LogManager.getLogger("SmallBabyHealth");
+    logger.info("GA GA!!!");
     return ResponseEntity.status(200).body("gaga!\n");
   }
 }
